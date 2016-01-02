76d0152915fbd2e8375838dabc6cf38de421d047
jip laptop <jhp.moors@gmail.com>
2016-01-02 20:48:02 +0100

    preparation for in-theme installation - without breaking plugin installed implementation

--- stencil-dependency.php
+++ stencil-dependency.php
@@ -3,7 +3,7 @@
  * Determine if Stencil is installed and register plugin if possible
  *
  * @package Stencil
- * @subpackage Twig
+ * @subpackage Smarty3
  */
 
 if ( ! function_exists( 'add_filter' ) ) {
@@ -16,36 +16,58 @@ if ( defined( 'STENCIL_PATH' ) ) {
 	$stencil_root = STENCIL_PATH;
 } else {
 	// Allow for fork of stencil to have different plugin base name.
-	$stencil_plugin_slug = apply_filters( 'stencil:plugin_slug', 'stencil' );
-	$stencil_root        = WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . $stencil_plugin_slug;
+	$stencil_directory = apply_filters( 'stencil:directory', 'stencil' );
+
+	// Prefer theme based stencil folder.
+	$theme = wp_get_theme();
+
+	$tests = array(
+		$theme->template_dir . DIRECTORY_SEPARATOR . $stencil_directory . DIRECTORY_SEPARATOR . $stencil_directory,
+		WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . $stencil_directory,
+	);
+
+	$stencil_root = false;
+
+	foreach ( $tests as $test ) {
+		if ( is_dir( $test ) ) {
+			$stencil_root = $test;
+			break;
+		}
+	}
 }
 
-$register = $stencil_root . DIRECTORY_SEPARATOR . 'register.php';
+/**
+ * If STENCIL_PATH is located inside a theme; we don't have to register.
+ */
+if ( false === strpos( $stencil_root, get_theme_root() ) ) {
 
-// Stencil can be found: register plugin.
-if ( is_file( $register ) ) {
+	$register = $stencil_root . DIRECTORY_SEPARATOR . 'register.php';
 
-	include $register;
+	// Stencil can be found: register plugin.
+	if ( is_file( $register ) ) {
 
-} else {
+		include $register;
 
-	if ( ! is_admin() ) {
-		return;
-	}
+	} else {
 
-	if ( ! function_exists( '__stencil_not_installed' ) ) {
-		/**
-		 * Notify the admin user that the Stencil plugin is required
-		 */
-		function __stencil_not_installed() {
-			$message = sprintf(
-				__( 'implementation: Required plugin "<a href="%s" target="_blank">Stencil</a>" is not installed.', 'stencil' ),
-				'https://wordpress.org/plugins/stencil/'
-			);
-
-			echo '<div class="error"><p>Stencil ' . $message . '</p></div>';
+		if ( ! is_admin() ) {
+			return;
 		}
 
-		add_action( 'admin_notices', '__stencil_not_installed' );
+		if ( ! function_exists( '__stencil_not_installed' ) ) {
+			/**
+			 * Notify the admin user that the Stencil plugin is required
+			 */
+			function __stencil_not_installed() {
+				$message = sprintf(
+					__( 'implementation: Required plugin "<a href="%s" target="_blank">Stencil</a>" is not installed.', 'stencil' ),
+					'https://wordpress.org/plugins/stencil/'
+				);
+
+				printf( '<div class="error"><p>Stencil %s</p></div>', $message );
+			}
+
+			add_action( 'admin_notices', '__stencil_not_installed' );
+		}
 	}
 }
--- stencil-twig.php
+++ stencil-twig.php
@@ -1,27 +1,27 @@
 <?php
 /**
-Plugin Name: Stencil: Twig Implementation
-Plugin URI: https://github.com/moorscode/stencil/
-Description: Twig Stencil Implementation. This plugin enables the use of Twig in your theme. This implementation requires the plugin "Stencil" to be installed and active.
-Version: 1.0.0
-Author: Jip Moors
-Author URI: http://www.jipmoors.nl
-Text Domain: stencil
-License: GPL2
-
-Stencil - Twig addon is free software: you can redistribute it
-and/or modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation, either version 2 of the
-License, or any later version.
-
-Stencil - Twig addon is distributed in the hope that it will
-be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Stencil - Twig addon.
-If not, see http://www.gnu.org/licenses/gpl-2.0.html.
+ * Plugin Name: Stencil: Twig Implementation
+ * Plugin URI: https://github.com/moorscode/stencil/
+ * Description: Twig Stencil Implementation. This plugin enables the use of Twig in your theme. This implementation requires the plugin "Stencil" to be installed and active.
+ * Version: 1.0.0
+ * Author: Jip Moors
+ * Author URI: http://www.jipmoors.nl
+ * Text Domain: stencil
+ * License: GPL2
+ *
+ * Stencil - Twig addon is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 2 of the
+ * License, or any later version.
+ *
+ * Stencil - Twig addon is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Stencil - Twig addon.
+ * If not, see http://www.gnu.org/licenses/gpl-2.0.html.
  *
  * @package Stencil
  * @subpackage Twig
@@ -33,40 +33,44 @@ if ( ! function_exists( 'add_filter' ) ) {
 	exit();
 }
 
-include( 'stencil-dependency.php' );
+if ( ! function_exists( '__stencil_twig_register' ) ) :
 
-add_filter( 'stencil:register-engine', '__stencil_twig_register' );
+	include( 'stencil-dependency.php' );
 
-/**
- * Register this implementation to Stencil
- *
- * @param array $engines Currently registered engines.
- *
- * @return array
- */
-function __stencil_twig_register( $engines ) {
-	$engine    = 'Twig';
-	$engines[] = $engine;
+	add_filter( 'stencil:register-engine', '__stencil_twig_register' );
 
-	add_action( 'stencil.activate-' . $engine, '__stencil_twig_activate' );
+	/**
+	 * Register this implementation to Stencil
+	 *
+	 * @param array $engines Currently registered engines.
+	 *
+	 * @return array
+	 */
+	function __stencil_twig_register( $engines ) {
+		$engine    = 'Twig';
+		$engines[] = $engine;
 
-	return $engines;
-}
+		add_action( 'stencil.activate-' . $engine, '__stencil_twig_activate' );
 
-/**
- * On activation, include engine file.
- */
-function __stencil_twig_activate() {
-	include_once( 'stencil-engine.php' );
-}
+		return $engines;
+	}
 
-/**
- * Load plugin textdomain
- */
-add_action(
-	'plugins_loaded',
-	create_function(
-		'',
-		"load_plugin_textdomain( 'stencil', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );"
-	)
-);
+	/**
+	 * On activation, include engine file.
+	 */
+	function __stencil_twig_activate() {
+		include_once( 'stencil-engine.php' );
+	}
+
+	/**
+	 * Load plugin textdomain
+	 */
+	add_action(
+		'plugins_loaded',
+		create_function(
+			'',
+			"load_plugin_textdomain( 'stencil', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );"
+		)
+	);
+
+endif;
